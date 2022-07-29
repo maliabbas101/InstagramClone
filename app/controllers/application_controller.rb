a6b26b1203d160349449fd292c3e5ce962bf3da3
muhammad-ali-abbas <106296517+muhammad-ali-abbas@users.noreply.github.com>
2022-07-29 18:04:25 +0500

    Implemented confirmable functionality.
    * user.rb : Added the confirmable.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(:email, :full_name, :username, :user_type, :password)
