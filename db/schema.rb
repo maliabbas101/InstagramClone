3bc08fee653f5ae83302830994408389436fdfad
muhammad-ali-abbas <106296517+muhammad-ali-abbas@users.noreply.github.com>
2022-07-29 13:35:34 +0500

    Implemented the user type.
    * application_controller.erb: Configured the devise.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_160730) do
+ActiveRecord::Schema.define(version: 2022_07_29_072739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2022_07_28_160730) do
     t.datetime "remember_created_at"
     t.string "full_name"
     t.string "username"
-    t.string "type"
+    t.string "user_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
