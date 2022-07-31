8447ed2f10e9ea0fbc72e17377a8557b31c3a5f8
muhammad-ali-abbas <106296517+muhammad-ali-abbas@users.noreply.github.com>
2022-08-01 02:12:59 +0500

    Implemented the images on Posts.
    * posts_controller: I whitelisted the images in params.
    * post.rb: I added has_many_attached :images.
    * Changed the form and views accordingly.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@
 
 class Post < ApplicationRecord
   belongs_to :user
+  has_many_attached :images
   validates :caption, presence: true
   validates :caption, length: { minimum: 5, maximum: 60 }
+  validates :images , length:{minimum:0 ,maximum:10, message: 'can not exceed 10 per post.' }
 end
