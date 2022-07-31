ede805c77c72c42c05b33f7b2727fc1f6dfae03b
muhammad-ali-abbas <106296517+muhammad-ali-abbas@users.noreply.github.com>
2022-08-01 03:06:25 +0500

    Rubocop
    * I did rubocop and deleted some unwanted files.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,43 +1,41 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[show edit update destroy]
   def index
     @comments = Comment.all
   end
 
-  def show
-
-  end
+  def show; end
 
   def new
     @comment = Comment.new
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to @comment , notice: 'Comment was successfully added.'
+      redirect_to @comment, notice: 'Comment was successfully added.'
     else
-      render 'new' , 'Something went wrong'
+      render 'new', 'Something went wrong'
     end
   end
 
   def update
-      if @comment.update_attributes(comment_params)
-        redirect_to @comment , notice: "Comment was successfully updated."
-      else
-        render 'edit' , notice: 'Something went wrong'
-      end
+    if @comment.update(comment_params)
+      redirect_to @comment, notice: 'Comment was successfully updated.'
+    else
+      render 'edit', notice: 'Something went wrong'
+    end
   end
 
   def destroy
     if @comment.destroy
       redirect_to comments_url, notice: 'Comment was successfully deleted.'
     else
-      redirect_to comments_url ,notice: 'Something went wrong'
+      redirect_to comments_url, notice: 'Something went wrong'
     end
   end
 
@@ -48,6 +46,6 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment).permit(:body , :user_id , :post_id)
+    params.require(:comment).permit(:body, :user_id, :post_id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,5 +6,5 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :caption, presence: true
   validates :caption, length: { minimum: 5, maximum: 60 }
-  validates :images , length:{minimum:0 ,maximum:10, message: 'can not exceed 10 per post.' }
+  validates :images, length: { minimum: 0, maximum: 10, message: 'can not exceed 10 per post.' }
 end
