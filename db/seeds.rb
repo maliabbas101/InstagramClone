f76f92ef85073bed5c75a3023678ac5043da0836
muhammad-ali-abbas <106296517+muhammad-ali-abbas@users.noreply.github.com>
2022-08-11 19:48:40 +0500

    Added side file data and did some error correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u1 = User.create(full_name: 'Ali Abbas', email: 'ali@gmail.com', password: '123456', username: 'ali_1234',
-                 user_type: 'Private')
-u1.stories << Story.create
-u2 = User.create(full_name: 'Ali', email: 'ali123@gmail.com', password: '123456', username: 'ali_12345',
-                 user_type: 'Public')
-u2.stories << Story.create
+u1 = User.create!(full_name: "Ali Abbas", email: "ali@gmail.com", password: "123456", username: "ali1",
+                  user_type: "Private")
+u1.skip_confirmation!
+u1.save!
+
+u2 = User.create!(full_name: "Ali Ahmed", email: "ahmed@gmail.com", password: "123456", username: "ahmed1",
+                  user_type: "Public")
+u2.skip_confirmation!
+u2.save!
+
+u3 = User.create!(full_name: "Usama Ahmed", email: "usama@gmail.com", password: "123456", username: "usama1",
+                  user_type: "Private")
+u3.skip_confirmation!
+u3.save!
+
+f1 = Friendship.create!(follower_id: u1.id, followed_id: u2.id)
+f1.save!
